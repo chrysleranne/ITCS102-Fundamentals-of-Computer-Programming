16b53a3e2843eedbac6386899dfa9ee3457e47b2
Chrysler Anne Idea <chryslerannei@gmail.com>
2025-09-10 20:55:27 +0800

    Add files via upload

--- activity10.py
+++ activity10.py
@@ -1,12 +1,13 @@
 from getpass import getpass
 
-username = 'chryslrnnd'
-password = 'sotdh'
+username = 'chryslrnnd' 
+password = 'hatdog'
 
-u = input("USERNAME - - >")
-p = getpass("PASSWORD - - >")
+u = input("USERNAME --> ")
+p = getpass("PASSWORD --> ")
 
 if(u == username) and (p == password) :
-		print("Access Granted")
+    print("Access Granted")
+
 else:
-		print("Access Denied")
+    print("Access Denied")
