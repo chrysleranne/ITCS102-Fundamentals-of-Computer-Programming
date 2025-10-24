0f4d6adf5b4991cdf4f07dfd8cbebc7fcc6d46d5
Chrysler Anne Idea <chryslerannei@gmail.com>
2025-10-24 13:52:22 +0800

    Add files via upload

---
+++ activity17.py
@@ -0,0 +1,4 @@
+for c in range(1,11,1):
+    for a in range(10,c,-1):
+        print(a, end=" ")
+    print( )
---
+++ activity18.py
@@ -0,0 +1,7 @@
+for c in range(1,11,1):
+    for x in range(10,c,-1):
+        print(" ",end=" ")
+    for y in range(1,c,1):
+        print("*",end=" ")
+    print( )
+    
